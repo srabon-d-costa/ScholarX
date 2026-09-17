1461d8e445c08e00c9c9f1e441a0983c8a1d3a31
Srabon D Costa <srabondcosta.ai@gmail.com>
2026-09-17 20:20:45 +0600

    Database connected successfully.

---
+++ config/database.php
@@ -0,0 +1,48 @@
+<?php
+
+class Database
+{
+
+    private $host = "localhost";
+    private $db_name = "scholarx";
+    private $username = "root";
+    private $password = "";
+
+    public $conn;
+
+
+    public function connect()
+    {
+
+        $this->conn = null;
+
+
+        try {
+
+            $this->conn = new PDO(
+                "mysql:host=".$this->host.";dbname=".$this->db_name,
+                $this->username,
+                $this->password
+            );
+
+
+            $this->conn->setAttribute(
+                PDO::ATTR_ERRMODE,
+                PDO::ERRMODE_EXCEPTION
+            );
+
+
+        } catch(PDOException $e){
+
+            echo "Connection Failed: " . $e->getMessage();
+
+        }
+
+
+        return $this->conn;
+
+    }
+
+}
+
+?>
